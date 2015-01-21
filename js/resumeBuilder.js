890051733d3bcd2596b2f000d7776a8fa0720937
Vincent Huang <vince@thosekids.me>
2015-01-20 22:36:21 -0800

    Add contact, picture, and skills

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -5,14 +5,42 @@ var newEmail = email.replace("udacity","gmail");
 console.log(email);
 console.log(newEmail);
 
-//*/
-var name = "Vincent Huang"
-var formattedName = HTMLheaderName.replace("%data%",name)
+var bio = {
+  "name" : "Vincent Huang",
+  "role" : "Product Manager",
+  "contacts" : {
+    "mobile" : "+1 415.547.0369",
+    "github" : "huangv22",
+    "twitter": "@huangv",
+    "location":"San Francisco",
+    "blog" :"http://www.vincentkardiogram.com/",
+  },
+  "welcomeMsg" : "Zig a Zig ahhh",
+  "skills" : ["Saying, good question", "Product Management"],
+  "bioPic":"images/me.png"
+}
 
-var role = "Product Manager"
-var formattedRole = HTMLheaderRole.replace("%data%",role);
+//*/
+var formattedName = HTMLheaderName.replace("%data%",bio.name);
+var formattedRole = HTMLheaderRole.replace("%data%",bio.role);
 
 $("#header").prepend(formattedRole).prepend(formattedName);
+
+var formattedMobile = HTMLmobile.replace("%data%", bio.contacts.mobile);
+var formattedTwitter = HTMLtwitter.replace("%data%",bio.contacts.twitter);
+var formattedgithub = HTMLgithub.replace("%data%",bio.contacts.github);
+var formattedlocation = HTMLlocation.replace("%data%",bio.contacts.location);
+var formattedblog = HTMLblog.replace("%data%",bio.contacts.blog);
+$("#topContacts").append(formattedlocation).append(formattedMobile).append(formattedTwitter)
+                 .append(formattedgithub).append(formattedblog);
+
+var formattedWelcomeMsg = HTMLWelcomeMsg.replace("%data%", bio.welcomeMsg);
+var formattedbioPic = HTMLbioPic.replace("%data%",bio.bioPic);
+var formattedskills = HTMLskills.replace("%data%",bio.skills)
+
+$("#header").append(formattedbioPic).append(formattedWelcomeMsg);
+$("#header").append(HTMLskillsStart).append(formattedskills);
+
 
 // $("#header").append(formmatedName);
 
